59cf156a979729b95fad40a524a846b043172121
DenisAlexandrov <aie-dinis2007@yandex.ru>
2024-04-12 22:57:14 +0300

    Initial Commit

---
+++ TrafficLights/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrafficLights
+//
+//  Created by Денис Александров on 12.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
